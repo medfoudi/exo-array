ebe8ffacf8eee0fd1e63e25413e9c0e9e0e9998b
medfoudi <foudimed52@gmail.com>
2023-08-04 12:56:26 +0200

    exo-array

---
+++ exo-array.js
@@ -0,0 +1,92 @@
+let myAlphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+  "$",
+  "*",
+  "/",
+  "-",
+  "+",
+]; 
+
+
+// function myAlphabetLength() {
+//     console.log(myAlphabet.length);
+// }
+// myAlphabetLength();
+
+// let planetArray = ["earth", "uranus", "mars", "venus", "mercure"];
+
+// planetArray.forEach((planet, index) => {
+//     console.log(planet, index);
+    // console.log(planet, planetArray.indexOf(planet))
+// });
+
+// let someDataTypes= ["Hello", 10, true, []];
+// someDataTypes.forEach(element => {
+//     console.log(typeof (element));
+//     console.log(someDataTypes.indexOf(element))
+// });
+
+// let fruits= []
+// // COPY / CLONE de fruits
+// console.log(fruits);
+// fruits.push("raisin", "banane");
+// let newfruits= [fruits];
+// console.log(newfruits);
+
+
+// let furnitures = ['Table', 'Chairs','Couch'];
+// let table= "table".split('');
+// console.log(table);
+// let Chairs= "chairs".split('');
+// console.log(Chairs);
+// let Couch= "couch".split('');
+// console.log(Couch);
+
+
+// let values1= ["Apple", 1, false, "2"];
+// let values2 = [`5`, "Fries", 2 , true];
+// let values3 = ["Mars", "Strawberry", 9];
+//     let globalevalues = values1.concat(values2).concat(values3);
+//     let numbervalues= globalevalues.filter ((element)=>typeof(element) === 'number');   
+//     console.log(numbervalues);
+
+
+// let studentCoursesA = ['Math', 'English', 'Programming'];
+// let studentCoursesB = ['Geography', 'Spanish', 'Programming'];
+// let stuudentCoursesC = studentCoursesA.concat(studentCoursesB);
+
+
+
+    
+
+
+
+
+    
+
+
